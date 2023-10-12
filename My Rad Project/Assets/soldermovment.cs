de8efbd37f3a69d41650bdce547e4fc4e5460e3c
DennisSave <Dennis.Saveljevs@students.ittralee.ie>
2023-10-12 16:44:05 +0100

    added jump and run using shift feature to character
    Signed-off-by: DennisSave <Dennis.Saveljevs@students.ittralee.ie>

--- My Rad Project/Assets/soldermovment.cs
+++ My Rad Project/Assets/soldermovment.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -7,12 +8,17 @@ public class soldermovment : MonoBehaviour
 {
     // Start is called before the first frame update
     Animator myAnimator;
-    float currentspeed, walkingspeed = 3, runningspeed = 6;
+    float jumpForce = 2;
+    float currentspeed, walkingspeed = 3;
+    float runningspeed, running = 6;
     private float turningSpeed = 180;
-
+    Rigidbody rb;
 
     void Start()
     {
+        
+        rb = GetComponent<Rigidbody>();
+        runningspeed = running;
         currentspeed = walkingspeed;
         myAnimator= GetComponent<Animator>();
     }
@@ -55,5 +61,35 @@ public class soldermovment : MonoBehaviour
             transform.Rotate(Vector3.up,  90* Time.deltaTime);
 
         }
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            myAnimator.SetBool("iswalking", true);
+            transform.position += runningspeed * transform.forward * Time.deltaTime;
+            
+        
+        
+        }
+
+        if(Input.GetKey(KeyCode.Space))
+        {
+
+
+            rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
+
+        }
+
+
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     }
 }
